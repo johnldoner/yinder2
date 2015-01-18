fdd90b047f6a6352fca16e7376e1f5345383bab6
Kevin Ji <kevin.ji@outlook.com>
2015-01-17 23:49:29 -0500

    Delete Matches section

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,10 +13,6 @@ angular.module('starter.controllers', ['ionic', 'ionic.contrib.ui.cards','fireba
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
-
 .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
   $scope.friend = Friends.get($stateParams.friendId);
 })
